7a2f6f1217c048cca6117da950b094eb90df49bd
Ameya Jathar <ameyaj20010@gmail.com>
2017-04-11 12:59:36 +0530

    Initial commit.

---
+++ SwiftShowcase/AppListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AppListCollectionViewCell.swift
+//  SwiftShowcase
+//
+//  Created by Ameya Jathar on 02/02/17.
+//  Copyright © 2017 Ameya. All rights reserved.
+//
+
+import UIKit
+
+class AppListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var appNameLabel: UILabel!
+    
+}
--- SwiftShowcase/ViewController.swift
+++ SwiftShowcase/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+enum SubApps: String {
+    case TraveLogr = "Travel log"
+}
 
+class ViewController: UICollectionViewController {
+
+    private let appList:[String] = ["Travel log"]
+    private let cellReusableIdentifier = "HomeCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: UICollectionViewDataSource methods
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return appList.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReusableIdentifier, for: indexPath) as! AppListCollectionViewCell
+        cell.appNameLabel.text = appList[indexPath.row]
+        return cell
+    }
+    
+    //MARK: UICollectionViewDelegate Methods
+    
 }
 
